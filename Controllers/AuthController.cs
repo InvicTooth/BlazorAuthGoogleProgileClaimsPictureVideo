94bc49ae0494169280e136cd80143ca487e7caff
InvicTooth <54736864+InvicTooth@users.noreply.github.com>
2023-10-06 23:24:57 +0900

    init

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Google;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorAuthGoogleProgileClaimsPictureVideo;
+
+[Route("/[controller]")]
+[ApiController]
+public class AuthController : ControllerBase
+{
+  [HttpGet("google-signin")]
+  public async Task<ActionResult> Google()
+  {
+    var properties = new AuthenticationProperties
+    {
+      RedirectUri = "/"
+    };
+    return Challenge(properties, GoogleDefaults.AuthenticationScheme);
+  }
+
+}
---
+++ Program.cs
@@ -0,0 +1,52 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using BlazorAuthGoogleProgileClaimsPictureVideo.Data;
+using BlazorAuthGoogleProgileClaimsPictureVideo;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddAuthentication("Cookies")
+  .AddCookie(opt => {
+    opt.Cookie.Name = "TryingoutGoogleOAuth";
+    opt.LoginPath = "/auth/google-signin";
+  })
+  .AddGoogle(opt => {
+    opt.ClientId = builder.Configuration["Google:Id"];
+    opt.ClientSecret = builder.Configuration["Google:Secret"];
+    opt.Scope.Add("profile");
+    opt.Events.OnCreatingTicket = context => {
+      string picuri = context.User.GetProperty("picture").GetString();
+      context.Identity.AddClaim(new System.Security.Claims.Claim("picture", picuri));
+      return Task.CompletedTask;
+    };
+  })
+  ;
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
